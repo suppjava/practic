27401ece052a1a621e3a3ee819da874e7770881a
suppjava <support@sarapul.town>
2019-06-04 14:24:58 +0400

    Второй коммит. Задание 3 выполнено

--- main.js
+++ main.js
@@ -1,18 +1,70 @@
-let money, time, appData, dayBudg, v1, v2;
-money = prompt("Ваш бюджет на месяц?", 20);
-time = prompt("Введите дату в формате YYYY-MM-DD", 2019-05-31);
-appData = {
+let money = +prompt("Ваш бюджет на месяц?", 20);
+let time = prompt("Введите дату в формате YYYY-MM-DD", 2019-05-31);
+
+let appData = {
   budget: money,
+  expenses: {},
+  optionalExpenses:{},
+  income:[],
   timeData: time,
-  expenses: {
-  },
-  optionalExpenses:1,
-  income:1,
   savings: false
 };
-v1 = prompt("Введите обязательную статью расходов в этом месяце, наименование", 0);
-v2 = prompt("Во сколько обойдется (сумма)?", 0);
-appData.expenses = {[v1] : v2};
-dayBudg = (money - appData.expenses[v1])/30;
-alert("ваш бюджет на 1 день состовляет:" + dayBudg);
+
+//appData.expenses = {[v1] : v2};
+
+
+/*for (let i = 0; i < 2; i++) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце, наименование", ''),
+      b = prompt("Во сколько обойдется (сумма)?", '');
+loop:  if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+  console.log("Ok, data");
+  appData.expenses[a] = b;
+} else {
+  alert("Введены не все данные");
+  --i;
+  break loop;
+}
+};*/
+/*let i = 0;
+while (i < 2) {
+  let a = prompt("Введите обязательную статью расходов в этом месяце, наименование", ''),
+  b = prompt("Во сколько обойдется (сумма)?", '');
+loop:  if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+  console.log("Ok, data");
+  appData.expenses[a] = b;
+} else {
+  alert("Введены не все данные");
+  --i;
+  break loop;
+}
+i++;
+};*/
+
+let i = 0;
+do {
+  let a = prompt("Введите обязательную статью расходов в этом месяце, наименование", ''),
+  b = prompt("Во сколько обойдется (сумма)?", '');
+loop:  if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+  console.log("Ok, data");
+  appData.expenses[a] = b;  
+} else {
+  alert("Введены не все данные");
+  --i;
+  break loop;
+}
+i++;
+} while (i < 2);
+
+appData.moneyPerDay = appData.budget/30;
+alert("ваш бюджет на 1 день состовляет:" + appData.moneyPerDay);
 console.log(appData);
+
+if (appData.moneyPerDay < 100) {
+  console.log("Ваш достаток минимален");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+  console.log("Ваш достаток средний");
+} else if (appData.moneyPerDay > 2000) {
+  console.log("Ваш достаток высок");
+} else {
+  console.log("Произошла ошибка");
+}
